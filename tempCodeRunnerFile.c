4cb609118b4a056ae2eec82c2f0fb7f59634235b
Thunderer0 <parilsanghvi0510@gmail.com>
2021-11-26 23:06:11 +0530

    basic library system
    basic usage of linked lists

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+printf("Enter the price of new book : ");
+    // scanf("%d",&price);
+    // printf("Enter the  name of new book : ");
+    // scanf("%[^\n]%*c",name);
+    // printf("Enter the author name of new book : ");
+    // scanf("%[^\n]%*c",&author);
+    // printf("Enter total of new book : ");
+    // scanf("%d",&total_pages);
